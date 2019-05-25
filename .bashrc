f8f5a4b1e8018c0fc2463f2af9a7a3a5121da90f
bosphere <boss.army@gmail.com>
2019-05-25 23:17:24 +0800

    add setup script

--- .bashrc
+++ .bashrc
@@ -6,9 +6,5 @@ if [ -f ~/.bash_android ]; then
     source ~/.bash_android
 fi
 
-if [ -f ~/.git-completion.bash ]; then
-    . ~/.git-completion.bash
-fi
-
 #PS1='\W \$ '
 PS1="\u@\[\e[0;32m\]\W\[\e[m\]\$ "
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+CUR_DIR=`dirname $0`
+
+function safecopy {
+    local FILE=`basename "$1"`
+    local OUTPUT_DIR=$2
+
+    if [[ ! -d "$OUTPUT_DIR" ]]; then
+        mkdir "$OUTPUT_DIR" || exit 1
+    fi
+    
+    if [[ -e "$OUTPUT_DIR/$FILE" ]]; then
+        echo "$OUTPUT_DIR/$FILE already exists, auto setup not possible"
+        exit 1
+    fi
+
+    cp "$CUR_DIR/$1" "$OUTPUT_DIR/$FILE"
+}
+
+safecopy bin/config_upstream.sh ~/bin
+safecopy bin/rebasemaster.sh ~/bin
+
+safecopy .git-completion.bash ~
+safecopy .bash_git ~
+
+safecopy .bash_android ~
+
+safecopy .bashrc ~
+safecopy .bash_profile ~
