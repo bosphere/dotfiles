88d6f9b78fec89c6b45525fb69afcd2418ffe001
bosphere <boss.army@gmail.com>
2019-11-26 23:33:10 +0800

    fix empty space in mp4 to gif convert script

--- bin/mp4_to_gif.sh
+++ bin/mp4_to_gif.sh
@@ -5,8 +5,8 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-ffmpeg -y -i $1 -vf fps=10,scale=320:-1:flags=lanczos,palettegen ${1}.palette.  png
+ffmpeg -y -i $1 -vf fps=10,scale=320:-1:flags=lanczos,palettegen ${1}.palette.png
 
-ffmpeg -y -i $1 -i ${1}.palette.png -filter_complex "fps=20,scale=320:-1:       flags=lanczos[x];[x][1:v]paletteuse" ${1}.gif
+ffmpeg -y -i $1 -i ${1}.palette.png -filter_complex "fps=20,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" ${1}.gif
 
 rm -rf ${1}.palette.png
